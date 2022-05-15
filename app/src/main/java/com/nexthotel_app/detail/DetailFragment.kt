26824174571db4fc8d06c54925f64e05a3e009de
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-05-15 20:23:19 +0700

    add detail hotel UI

---
+++ app/src/main/java/com/nexthotel_app/detail/DetailFragment.kt
@@ -0,0 +1,24 @@
+package com.nexthotel_app.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nexthotel_app.R
+
+
+class DetailFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail, container, false)
+    }
+
+}
