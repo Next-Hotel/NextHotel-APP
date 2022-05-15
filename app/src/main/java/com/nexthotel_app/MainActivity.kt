a78c95826303d2f6c5ae02c3821b451c14e60dc2
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-05-15 22:57:38 +0700

    Home (#3)
    Co-authored-by: gonexwind <fikky.ardi15@gmail.com>

--- app/src/main/java/com/nexthotel_app/MainActivity.kt
+++ app/src/main/java/com/nexthotel_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.nexthotel_app
 
 import android.os.Bundle
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
@@ -14,7 +15,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        
         val navHostFragment = supportFragmentManager
             .findFragmentById(R.id.navHostFragment) as NavHostFragment
         navController = navHostFragment.navController
