352b472e92ea8fee89645c94f6bee0485899918b
Faiz Rofi Hencya <faiz.rofi01@gmail.com>
2022-05-15 22:50:48 +0700

    implement splash screen

--- app/src/main/java/com/nexthotel_app/splashScreen/SplashScreenFragment.kt
+++ app/src/main/java/com/nexthotel_app/splashScreen/SplashScreenFragment.kt
@@ -1,23 +1,32 @@
 package com.nexthotel_app.splashScreen
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.nexthotel_app.R
 
+@SuppressLint("CustomSplashScreen")
 class SplashScreenFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        (activity as AppCompatActivity).supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+        }, 2000)
     }
 }
